01105fa938566d15f683ae77dbaef2e020694ce7
hewsontrinh526 <6734@holbertonstudents.com>
2023-07-25 02:56:28 -0700

    Feat: Added write_str function

--- form_func.c
+++ form_func.c
@@ -14,3 +14,23 @@ int write_char(va_list args)
   _putchar(c);
   return (1);
 }
+
+/**
+*
+*/
+int write_str(va_list args)
+{
+	char *s;
+	int i;
+	int len;
+
+	i = 0;
+	s = va_arg(args, char *);
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i = i + 1;
+	}
+	return (i);
+}
