b5fc82b4f02dc3177e3c0b33e0b943d37cd9baff
root <6721@holbertonstudents.com>
2023-07-26 02:27:48 -0700

    style: betty, doc: added descriptions

--- _printf.c
+++ _printf.c
@@ -6,53 +6,49 @@
 
 /**
 * _printf - prints anything
+* @format: the user input
 *
+* Return: the number of characters printed
 */
 int _printf(const char *format, ...)
 {
-	int i;
-	int printed;
+	int i = 0, printed = 0;
 	va_list args;
 	int (*res)(va_list);
-
-	i = 0;
-	printed = 0;
 
 	if (format == NULL)
 	{
 		return (-1);
 	}
-
 	va_start(args, format);
-
 	while (format[i] != '\0')
 	{
-      if (format[i] == '%')
-      {
-	      if (format[i + 1] == '\0')
-	      {
-		      va_end(args);
-		      return (-1);
-	      }
-	      res = get_form_func(format[i + 1]);
-	      if (res == NULL)
-	      {
-		      _putchar('%');
-		      _putchar(format[i + 1]);
-		      printed = printed + 2;
-	      }
-	      else
-	      {
-		      printed = printed + (*res)(args);
-	      }
-	      i = i + 1;
-      }
-      else
-      {
-	      _putchar(format[i]);
-	      printed = printed + 1;
-      }
-      i = i + 1;
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == '\0')
+			{
+				va_end(args);
+				return (-1);
+			}
+			res = get_form_func(format[i + 1]);
+			if (res == NULL)
+			{
+				_putchar('%');
+				_putchar(format[i + 1]);
+				printed = printed + 2;
+			}
+			else
+			{
+				printed = printed + (*res)(args);
+			}
+			i = i + 1;
+		}
+		else
+		{
+			_putchar(format[i]);
+			printed = printed + 1;
+		}
+		i = i + 1;
 	}
 	va_end(args);
 	return (printed);
