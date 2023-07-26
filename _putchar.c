c7c726bab632964b603c7113df927304e8bbbedb
root <6721@holbertonstudents.com>
2023-07-26 02:38:26 -0700

    style: betty, doc: added description for putchar

--- _putchar.c
+++ _putchar.c
@@ -1,11 +1,12 @@
 #include <unistd.h>
 
 /**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- *
- *
+ * Return: 1 if success, -1 if error
  */
 int _putchar(char c)
 {
-        return (write(1, &c, 1));
+	return (write(1, &c, 1));
 }
